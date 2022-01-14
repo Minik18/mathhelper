0f39bc9b83967bfb1e18ce48e99a2e648ed1e9c6
fakodominik <fakodominik@gmail.com>
2022-01-14 16:03:36 +0100

    Added blank page

---
+++ src/main/java/hu/unideb/inf/mathhelper/MathHelperApplication.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf.mathhelper;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MathHelperApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
