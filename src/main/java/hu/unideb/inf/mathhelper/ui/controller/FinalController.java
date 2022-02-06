dd5f63479b1f5f05963e575ca6b32530ad646806
Dominik Fako <fakodominik@gmail.com>
2022-02-06 15:47:16 +0100

    Started final

---
+++ src/main/java/hu/unideb/inf/mathhelper/ui/controller/FinalController.java
@@ -0,0 +1,10 @@
+package hu.unideb.inf.mathhelper.ui.controller;
+
+import javafx.stage.Stage;
+
+public class FinalController implements Controller{
+    @Override
+    public void setup(Stage stage) {
+
+    }
+}
